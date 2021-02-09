d0da40ce0a620f07f6459940cf4d9665197eb5bf
F9y4ng <f9y4ng@gmail.com>
2021-02-09 18:45:28 +0800

    update

--- Font Rendering.user.js
+++ Font Rendering.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name            字体渲染（自用脚本）
 // @namespace       https://openuserjs.org/users/t3xtf0rm4tgmail.com
-// @version         2021.02.08.1
+// @version         2021.02.09.1
 // @icon            https://github.githubassets.com/favicons/favicon.svg
 // @description     让每个页面的字体变得有质感，默认使用苹方字体，附加字体描边、字体阴影、字体平滑等效果，自用脚本不处理外部需求。
 // @supportURL      https://github.com/F9y4ng/GreasyFork-Scripts/issues
@@ -25,9 +25,9 @@
   /* 你可以自定义以下内容 */
 
   let stroke_r = 0.1; // 字体描边：建议控制在0~1.0之间，关闭描边为0，默认0.1
-  let shadow_r = 4; // 字体阴影：建议控制在4~8之间，关闭阴影为0，默认4
+  let shadow_r = 4; // 字体阴影：建议控制在1~8之间，关闭阴影为0，默认4
   const smooth_i = 1; // 字体平滑，默认开启，关闭为0
-  const shadow_c = `#888`; // 阴影颜色：建议#777,#888,#999，或 rgba(136,136,136,0.8) 或依喜好修改，currentcolor为原字体颜色（慎用）
+  const shadow_c = `#888`; // 阴影颜色：建议#888，或 rgba(136,136,136,0.8) 或依喜好修改，currentcolor为原字体颜色（慎用）
   const cssfun = `body:not(input):not(textarea):not(i):not(em):not(pre):not(code):not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"])`; // 可以继续添加需要屏蔽的标签或classname或ID
 
   /* 请勿修改以下代码 */
@@ -37,7 +37,7 @@
   let shadow = '';
   shadow_r = parseFloat(shadow_r);
   if (!isNaN(shadow_r) && shadow_r !== 0) {
-    shadow = `text-shadow: 1px 1px ${shadow_r}px ${shadow_c}!important;`;
+    shadow = `text-shadow: 0 0 ${shadow_r}px ${shadow_c}!important;`;
   }
   let stroke = '';
   stroke_r = parseFloat(stroke_r);
