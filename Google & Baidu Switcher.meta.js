d9e24e898fc700edc95050c46aa5b96893631da8
F9y4ng <f9y4ng@gmail.com>
2021-05-28 22:52:03 +0800

    update

---
+++ Google & Baidu Switcher.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name            Google & baidu Switcher (ALL in One)
+// @version         2.4.20210527.15
+// @author          F9y4ng
+// @license         GPL-3.0-only
+// @copyright       2015-2021, F9y4ng
+// ==/UserScript==
