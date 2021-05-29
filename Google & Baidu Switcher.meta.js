c2065886755155db6db2482540657aacf6dfdc8a
F9y4ng <f9y4ng@gmail.com>
2021-05-29 19:16:26 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version    2.4.20210529.7
+// @version    2.4.20210529.8
 // @name    Google & baidu Switcher (ALL in One)
 // @name:en    Google & baidu & Bing Switcher (ALL in One)
 // @name:zh-TW    谷歌搜索、百度搜索、必應搜索的聚合跳轉集合工具
