7b8fa7890d5308ae8a3149c38ac8cb91e2f19b5d
F9y4ng <f9y4ng@gmail.com>
2021-06-06 18:28:06 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @version     3.0.20210606.1
+// @version     3.0.20210606.2
 // @name        Google & baidu Switcher (ALL in One)
 // @name:en     Google & baidu & Bing Switcher (ALL in One)
 // @name:zh-TW  谷歌搜索、百度搜索、必應搜索的聚合跳轉集合工具
 // @namespace   https://openuserjs.org/scripts/t3xtf0rm4tgmail.com/Google_baidu_Switcher_(ALL_in_One)
-// @note        1、新增animate.css，压缩CSS.\n2、新增更新内容提示。\n3、修正bugs.
+// @note        新增animate动画效果，压缩代码中的CSS.\n新增更新内容提示。\n修正一些bugs.
 // ==/UserScript==
