16999c76bc92e0d62002c08a9a33ea7d01239c0f
F9y4ng <f9y4ng@gmail.com>
2021-07-03 14:04:59 +0800

    update

--- Google & Baidu Switcher.user.js
+++ Google & Baidu Switcher.user.js
@@ -3,7 +3,7 @@
 // @name            Google & baidu Switcher (ALL in One)
 // @name:en         Google & baidu & Bing Switcher (ALL in One)
 // @name:zh-TW      谷歌、百度、必應的搜索引擎跳轉工具
-// @version         2021.07.03.1
+// @version         3.3.20210703.1
 // @author          F9y4ng
 // @description     谷歌、百度、必应的搜索引擎跳转工具，脚本默认自动更新检测，可在菜单自定义设置必应按钮，搜索引擎跳转的最佳体验。
 // @description:en  Google, Baidu and Bing search engine tool, Automatically updated and detected by default, The Bing button can be customized.
