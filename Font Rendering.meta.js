2c5caefda2c74ce3f6ef5deb72b7fd183fef3969
F9y4ng <f9y4ng@gmail.com>
2021-07-04 12:34:29 +0800

    update

--- Font Rendering.meta.js
+++ Font Rendering.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          字体渲染（自用脚本）
-// @version       2021.07.03.2
+// @version       2021.07.04.1
 // @author        F9y4ng
 // @namespace     https://openuserjs.org/scripts/f9y4ng/Font_Rendering_(Customized)
 // ==/UserScript==
