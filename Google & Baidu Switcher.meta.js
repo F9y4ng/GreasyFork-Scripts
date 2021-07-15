744d0ffff2c9904655d804f6bc0b0a0f83df1456
F9y4ng <f9y4ng@gmail.com>
2021-07-15 16:23:58 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -3,8 +3,8 @@
 // @name:en         Google & baidu & Bing Switcher (ALL in One)
 // @name:zh         谷歌、百度、必应的搜索引擎跳转工具
 // @name:zh-TW      谷歌、百度、必應的搜索引擎跳轉工具
-// @version     3.3.20210713.2
+// @version     3.3.20210715.2
 // @author      F9y4ng
 // @namespace   https://openuserjs.org/scripts/f9y4ng/Google_baidu_Switcher_(ALL_in_One)
-// @note        修正google no country redirect的判断的bug。\n修正bug，优化代码。
+// @note        修正google ncr的判断在firefox的兼容性，以及其他脚本插件下的兼容性。\n修正bug，优化代码。
 // ==/UserScript==
