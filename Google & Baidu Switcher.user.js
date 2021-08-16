f672fa733f3184af79e8cfd22d7bfdd44aaf4403
F9y4ng <f9y4ng@gmail.com>
2021-08-16 21:48:54 +0800

    update

--- Google & Baidu Switcher.user.js
+++ Google & Baidu Switcher.user.js
@@ -4,7 +4,7 @@
 // @name:en         Google & baidu & Bing Switcher (ALL in One)
 // @name:zh         谷歌、百度、必应的搜索引擎跳转工具
 // @name:zh-TW      谷歌、百度、必應的搜索引擎跳轉工具
-// @version         3.6.20210814.1
+// @version         3.6.20210816.1
 // @author          F9y4ng
 // @description         谷歌、百度、必应的搜索引擎跳转工具，脚本默认自动更新检测，可在菜单自定义设置必应按钮，搜索引擎跳转的最佳体验。
 // @description:en      Google, Baidu and Bing search engine tool, Automatically updated and detected by default, The Bing button can be customized.
@@ -29,7 +29,7 @@
 // @compatible      Firefox 兼容Greasemonkey4.0+, TamperMonkey, ViolentMonkey
 // @compatible      Opera 兼容TamperMonkey, ViolentMonkey
 // @compatible      Safari 兼容Tampermonkey • Safari
-// @note            发布Release版本。\n修正bugs，优化代码。
+// @note            修正bugs，优化代码。
 // @grant           GM_info
 // @grant           GM_registerMenuCommand
 // @grant           GM.registerMenuCommand
@@ -134,6 +134,7 @@
     warning: defCon.randString(7, true),
     info: defCon.randString(7, true),
     error: defCon.randString(7, true),
+    checkbox: defCon.randString(6, true),
     configuration: defCon.randString(7, true),
     fc: defCon.randString(8, true),
     fcSave: defCon.randString(5, true),
@@ -744,8 +745,8 @@
         sourceSite = sourceSite.replace("_cdn", ".CDN");
         sourceSite = cache ? `${sourceSite} on Cache` : sourceSite;
         const repo = cache
-          ? `\nCache expire:${defCon.durationTime(defCon.restTime)}\nDetection: ${defCon.lastRuntime()}\n`
-          : `\nExpiration: ${_expire_time_}\nDetection: ${defCon.lastRuntime()}\n`;
+          ? `\nCacheExpire:${defCon.durationTime(defCon.restTime)}\nDetectTime: ${defCon.lastRuntime()}\n`
+          : `\nExpireTime: ${_expire_time_}\nDetectTime: ${defCon.lastRuntime()}\n`;
         const sourceURL = recheckURLs.toString().replace("_cdn", "");
         switch (defCon.isNeedUpdate) {
           case 2:
