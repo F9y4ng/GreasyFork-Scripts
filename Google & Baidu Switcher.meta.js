45d6e23d1c94c7eb2c8bceb86c3773c902df97c7
F9y4ng <f9y4ng@gmail.com>
2021-10-16 21:00:18 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -4,8 +4,8 @@
 // @name:zh-TW    谷歌、百度、必應的搜索引擎跳轉工具
 // @name:en       Google & baidu & Bing Switcher (ALL in One)
 // @name:ja       Google、Baidu、Bingの検索エンジンのジャンプツール
-// @version       3.8.20211012.1
+// @version       3.8.20211016.1
 // @author        F9y4ng
 // @namespace     https://openuserjs.org/scripts/f9y4ng/Google_baidu_Switcher_(ALL_in_One)
-// @note          修正NoticeJs类的bugs，提高兼容性。\n修正关键词高亮的bugs.\n修正其他bugs, 优化代码。
+// @note          新增当前浏览器窗口跳转功能，如需使用，请在设置中开启。
 // ==/UserScript==
