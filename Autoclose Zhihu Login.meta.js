92187c059af01eda7d075d0f877e196a62eec1e0
F9y4ng <F9y4ng@gmail.com>
2024-01-01 13:39:01 +0800

    update

--- Autoclose Zhihu Login.meta.js
+++ Autoclose Zhihu Login.meta.js
@@ -1,6 +1,9 @@
 // ==UserScript==
 // @name         自动关闭知乎登录提示
-// @version      2023.11.04.1
+// @name:en      Autoclose Zhihu Login Prompt
+// @name:zh-CN   自动关闭知乎登录提示
+// @name:zh-TW   自動關閉知乎登錄提示
+// @version      2024.01.01.1
 // @author       F9y4ng
 // @namespace    https://github.com/F9y4ng/GreasyFork-Scripts/
 // @description  自动关闭知乎自动弹出的登录与注册提示，仅仅用于关闭自动弹出的登录提示，不干别的，未来也不会去干别的。
