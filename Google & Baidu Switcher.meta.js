54bc95c87df884b06f132f2b1f8f310fceea6846
F9y4ng <F9y4ng@gmail.com>
2024-01-01 13:39:38 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -1,18 +1,17 @@
 // ==UserScript==
 // @name         Google & baidu Switcher (ALL in One)
-// @name:en      SearchEngine Assistant
-// @name:zh-CN   优雅的搜索引擎跳转助手
-// @name:zh-TW   優雅的搜索引擎跳轉助手
-// @name:ru      скачок поисковой системы
-// @name:ja      優雅な検索エンジンジャンプ助手
-// @version      2023.12.02.1
+// @name:en      Search Engine Assistant
+// @name:zh-CN   优雅的搜索引擎助手
+// @name:zh-TW   優雅的搜索引擎助手
+// @name:ru      Поисковый помощник
+// @name:ja      優雅な検索エンジン助手
+// @version      2024.01.01.1
 // @author       F9y4ng
-// @description  "Elegant search engine Jump Assistant" facilitates users to jump between different search engines; supports custom commonly used search engines and search keyword highlighting effects; provides advanced functions such as removing search link redirection, blocking search results advertisements, etc.; it is compatible with well-known search engines such as Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Ecosia, You, Startpage, Brave, etc.
+// @description  "Elegant search engine assistant" facilitates users to jump between different search engines; supports custom commonly used search engines and search keyword highlighting effects; provides advanced functions such as removing search link redirection, blocking search results advertisements, etc.; it is compatible with well-known search engines such as Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Ecosia, You, Startpage, Brave, etc.
 // @namespace    https://openuserjs.org/scripts/f9y4ng/Google_baidu_Switcher_(ALL_in_One)
-// @note         更新greasyfork.org新版本@require地址。
-// @note         新增搜索结果过滤功能，拒绝垃圾与内容农场。
-// @note         移除失效的Fsou,新增中文搜索：无追搜索。
-// @note         重构去重定向功能，优化重定向链接解析效率。
-// @note         重构去广告功能，优化广告屏蔽效率和效果。
+// @note         ✨🎉祝新年快乐，身体健康，万事如意🎉✨
+// @note         优化部分搜索引擎的去广告功能。
+// @note         优化搜索结果关键词过滤的检测效果。
+// @note         提升对更多浏览器及脚本管理器的兼容性。
 // @note         修正一些已知问题，优化样式，优化代码。
 // ==/UserScript==
