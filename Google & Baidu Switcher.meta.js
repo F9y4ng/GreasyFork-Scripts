b385bdea70ad7b675e6e6f675e18183029cf2b4b
F9y4ng <F9y4ng@gmail.com>
2024-02-03 13:41:19 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -5,13 +5,11 @@
 // @name:zh-TW   優雅的搜索引擎助手
 // @name:ru      Поисковый помощник
 // @name:ja      優雅な検索エンジン助手
-// @version      2024.01.01.1
+// @version      2024.02.03.1
 // @author       F9y4ng
 // @description  "Elegant search engine assistant" facilitates users to jump between different search engines; supports custom commonly used search engines and search keyword highlighting effects; provides advanced functions such as removing search link redirection, blocking search results advertisements, etc.; it is compatible with well-known search engines such as Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Ecosia, You, Startpage, Brave, etc.
 // @namespace    https://openuserjs.org/scripts/f9y4ng/Google_baidu_Switcher_(ALL_in_One)
-// @note         ✨🎉祝新年快乐，身体健康，万事如意🎉✨
-// @note         优化部分搜索引擎的去广告功能。
-// @note         优化搜索结果关键词过滤的检测效果。
-// @note         提升对更多浏览器及脚本管理器的兼容性。
+// @note         修复部分搜索引擎的跳转按钮的显示BUG。
+// @note         置换脚本引用源地址至github.com域内。
 // @note         修正一些已知问题，优化样式，优化代码。
 // ==/UserScript==
