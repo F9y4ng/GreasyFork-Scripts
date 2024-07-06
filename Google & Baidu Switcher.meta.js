d5199db2e6e3c570277b47140d0fb3e2cafa5890
F9y4ng <F9y4ng@gmail.com>
2024-07-06 15:10:53 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -2,13 +2,19 @@
 // @name         Google & baidu Switcher (ALL in One)
 // @name:en      Search Engine Assistant
 // @name:zh-CN   优雅的搜索引擎助手
-// @name:zh-TW   優雅的搜索引擎助手
-// @name:ru      Поисковый помощник
+// @name:zh-TW   優雅的搜尋引擎助手
+// @name:ru      помощник поисковых систем
 // @name:ja      優雅な検索エンジン助手
-// @version      2024.06.01.1
+// @version      2024.07.06.1
 // @author       F9y4ng
-// @description  "Elegant search engine assistant" facilitates users to jump between different search engines; supports custom commonly used search engines and search keyword highlighting effects; provides advanced functions such as removing search link redirection, blocking search results advertisements, etc.; it is compatible with well-known search engines such as Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Ecosia, You, Startpage, Brave, etc.
+// @description  "Elegant search engine assistant" allows switching between engines; supports custom engines, keyword highlighting; offers redirect removal, ad blocking, keyword filtering, and auto-updates; compatible with Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Qwant, Ecosia, You, Startpage, Brave, Yahoo, Yep, Swisscows, searXNG and more.
 // @namespace    https://openuserjs.org/scripts/f9y4ng/Google_baidu_Switcher_(ALL_in_One)
-// @note         {"CN":"修正Google搜索分页链接开新窗口的问题。","EN":"Fixed the issue where Google search pagination links opened in a new window."}
+// @note         {"CN":"删除已失效的无追搜索跳转及相关功能。","EN":"Removed invalid search engine and related feats."}
+// @note         {"CN":"新增 SearXNG 搜索引擎的跳转及相关功能。","EN":"Added SearXNG search engine and related feats."}
+// @note         {"CN":"新增 Qwant 搜索引擎的跳转及相关功能。","EN":"Added Qwant search engine and related feats."}
+// @note         {"CN":"修正 Yandex/Brave 搜索引擎跳转按钮的错误。","EN":"Fixed Yandex/Brave search engine button errors."}
+// @note         {"CN":"修正 Bing 搜索链接重定向错误及样式异常。","EN":"Fixed Bing link redirection and style errors."}
+// @note         {"CN":"修正 Tampermonkey5.2.1 去除百度重定向的错误。","EN":"Fixed the bug of removing Baidu link redirection in Tampermonkey 5.2.1."}
+// @note         {"CN":"修正 Yahoo 国家子域名下跳转按钮未加载的问题。","EN":"Fixed Yahoo.Search jump buttons not load under the country subdomain."}
 // @note         {"CN":"修正一些已知问题，优化代码，优化样式。","EN":"Fixed known issues, optimize code and style."}
 // ==/UserScript==
