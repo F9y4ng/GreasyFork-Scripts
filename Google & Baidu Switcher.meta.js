f11caf4276978d930835bb5ff7dab87a2a9e7ad4
F9y4ng <15855068+F9y4ng@users.noreply.github.com>
2025-02-08 15:00:42 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -5,12 +5,10 @@
 // @name:zh-TW   優雅的搜尋引擎助手
 // @name:ru      помощник поисковых систем
 // @name:ja      優雅な検索エンジン助手
-// @version      2025.01.01.1
+// @version      2025.02.08.1
 // @author       F9y4ng
 // @description  "Elegant search engine assistant" allows switching between engines; supports custom engines, keyword highlighting; offers redirect removal, ad blocking, keyword filtering, and auto-updates; compatible with Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Qwant, Ecosia, You, Startpage, Brave, Yahoo, Yep, Swisscows, searXNG and more.
 // @namespace    https://openuserjs.org/scripts/f9y4ng/Google_baidu_Switcher_(ALL_in_One)
-// @note         {"CN":"✨ 恭祝大家新年快乐，身体健康、平安如意。🎉","EN":"✨ Wishing you a happy New Year, good health, peace and all the best. 🎉 Blessings via F9y4ng."}
-// @note         {"CN":"年度更新脚本版权日期信息，已服务 10 年啦！","EN":"Updated script copyright date to the 10th year."}
-// @note         {"CN":"优化 cn.Bing 搜索结果广告的移除规则。","EN":"Optimized cn.Bing search results ads removal."}
+// @note         {"CN":"修正 Google 搜索跳转按钮样式问题。","EN":"Fixed Google Search Jump Button style issue."}
 // @note         {"CN":"修正一些已知问题，优化代码，优化样式。","EN":"Fixed some known issues, optimized code & style."}
 // ==/UserScript==
