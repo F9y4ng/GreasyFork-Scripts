7b7d75f3c57acc075d824150bb4e508118987e4b
F9y4ng <15855068+F9y4ng@users.noreply.github.com>
2025-11-01 13:47:04 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -5,14 +5,13 @@
 // @name:zh-TW   優雅的搜尋引擎助手
 // @name:ru      Помощник поисковой системы
 // @name:ja      優雅な検索エンジン助手
-// @version      2025.10.05.1
+// @version      2025.11.01.1
 // @author       F9y4ng
 // @description  "Elegant search engine assistant" allows switching between engines; supports custom engines, keyword highlighting; offers redirect removal, ad blocking, keyword filtering, and auto-updates; compatible with Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Qwant, Ecosia, You, Startpage, Brave, Yahoo, Yep, Mojeek, searXNG and more.
 // @namespace    https://github.com/F9y4ng/GreasyFork-Scripts/
-// @note         {"CN":"优化部分搜索引擎广告屏蔽规则。","EN":"Optimized some search engine ad blocking rules."}
-// @note         {"CN":"优化搜索结果链接重定向检测机制。","EN":"Optimized search result link redirect detection."}
-// @note         {"CN":"优化对搜索链接重定向地址的关键词拦截。","EN":"Optimized keyword block for search redirect url."}
-// @note         {"CN":"修复 Bing.com 链接重定向被监测还原的问题。","EN":"Fixed Bing.com link redirection restore issue."}
-// @note         {"CN":"修复 Google.com 搜索链接的颜色问题。","EN":"Fixed color issue with Google.com search links."}
+// @note         {"CN":"优化重构脚本核心函数，提升运行效率。","EN":"Refactored core functions to improve efficiency."}
+// @note         {"CN":"重构搜索引擎跳转按钮的载入模式。","EN":"Refactored the loading mode of jump buttons."}
+// @note         {"CN":"优化所有搜索引擎跳转按钮的实时样式。","EN":"Optimize the real-time styles of all jump buttons."}
+// @note         {"CN":"完美解决暗黑模式按钮及链接的颜色问题。","EN":"Perfect solution for button issues in darkmode."}
 // @note         {"CN":"修正一些已知问题，优化代码，优化样式。","EN":"Fixed some known issues, optimized code & style."}
 // ==/UserScript==
