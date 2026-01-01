8c730edb3c91c79a326042fbdac658a4d303ca8f
F9y4ng <15855068+F9y4ng@users.noreply.github.com>
2026-01-01 14:29:43 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -5,13 +5,13 @@
 // @name:zh-TW   優雅的搜尋引擎助手
 // @name:ru      Помощник поисковой системы
 // @name:ja      優雅な検索エンジン助手
-// @version      2025.12.06.1
+// @version      2026.01.01.1
 // @author       F9y4ng
 // @description  "Elegant search engine assistant" allows switching between engines; supports custom engines, keyword highlighting; offers redirect removal, ad blocking, keyword filtering, and auto-updates; compatible with Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Qwant, Ecosia, You, Startpage, Brave, Yahoo, Yep, Mojeek, searXNG and more.
 // @namespace    https://github.com/F9y4ng/GreasyFork-Scripts/
-// @note         {"CN":"优化脚本核心代码，兼容更多脚本管理器。","EN":"Optimized code for compatibility with more script managers."}
-// @note         {"CN":"优化 Google 各种灰度测试版本的滚动兼容。","EN":"Optimized scroll compatibility for Google A/B test."}
-// @note         {"CN":"优化 Bing.com 暗黑模式的搜索链接颜色。","EN":"Optimized search link color for Bing dark mode."}
-// @note         {"CN":"优化脚本样式的载入模式，提升性能及兼容性。","EN":"Optimize the loading mode of script styles."}
+// @note         {"CN":"🎉恭祝各位用户 𝟐𝟎𝟐𝟔 新年快乐，万事如意。🎊","EN":"🎉Wishing all users a happy New Year in 𝟐𝟎𝟐𝟔.🎊"}
+// @note         {"CN":"更新 2026 年度脚本版权信息（第十一年度）。","EN":"Updated script copyright information in 2026."}
+// @note         {"CN":"修复 Bing.com 新的灰度测试中搜索过滤问题。","EN":"Fixed search filter issue in Bing's A/B test."}
+// @note         {"CN":"修复部分搜索引擎跳转按钮的样式问题。","EN":"Fixed jump buttons issue of some search engine."}
 // @note         {"CN":"修正一些已知问题，优化代码，优化样式。","EN":"Fixed some known issues, optimized code & style."}
 // ==/UserScript==
