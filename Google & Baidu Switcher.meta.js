4134b15a371ae31785d2f644335b43e2c95619f0
F9y4ng <15855068+F9y4ng@users.noreply.github.com>
2026-04-04 14:38:39 +0800

    update

--- Google & Baidu Switcher.meta.js
+++ Google & Baidu Switcher.meta.js
@@ -5,10 +5,13 @@
 // @name:zh-TW   優雅的搜尋引擎助手
 // @name:ru      Помощник поисковой системы
 // @name:ja      優雅な検索エンジン助手
-// @version      2026.03.01.1
+// @version      2026.04.04.1
 // @author       F9y4ng
 // @description  "Elegant search engine assistant" allows switching between engines; supports custom engines, keyword highlighting; offers redirect removal, ad blocking, keyword filtering, and auto-updates; compatible with Baidu, Google, Bing, Duckduckgo, Yandex, Sogou, Qwant, Ecosia, You, Startpage, Brave, Yahoo, Yep, Mojeek, searXNG and more.
 // @namespace    https://github.com/F9y4ng/GreasyFork-Scripts/
-// @note         {"CN":"修正 Bing 跳转按钮因屏幕尺寸差异引发的错位。","EN":"Fixed misalignment of Bing jump buttons caused by screen size differences."}
+// @note         {"CN":"新增双击脚本标题栏查看更新记录。","EN":"Added double-click title to view update history."}
+// @note         {"CN":"新增搜索结果屏蔽关键词添加工具。","EN":"Added search result blocking keyword adding tool."}
+// @note         {"CN":"优化部分搜索引擎跳转按钮样式。","EN":"Optimized some search engine jump button styles."}
+// @note         {"CN":"强制 replaceSync 接受 TrustedScript 类型。","EN":"Force replaceSync to accept TrustedScript types."}
 // @note         {"CN":"修正一些已知问题，优化代码，优化样式。","EN":"Fixed some known issues, optimized code & style."}
 // ==/UserScript==
